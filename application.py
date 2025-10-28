b0c2aba811ca09b06c20d2d34967e9e66ac96e1e
juljul22 <julie.joan@web.de>
2025-10-28 17:56:29 +0100

    Update application.py

--- application.py
+++ application.py
@@ -1,4 +1,14 @@
-from capp import application
+from flask import Flask, render_template, url_for
+application = Flask(__name__)
+
+@application.route('/')
+@application.route('/home')
+def home():
+    return render_template('home.html')
+
+@application.route('/methodology')
+def methodology():
+    return render_template('methodology.html')
 
 if __name__=='__main__':
-  application.run(debug=True)  
+    application.run(debug=True)  
