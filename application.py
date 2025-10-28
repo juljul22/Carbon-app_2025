88e734d6c6cbc595fe4cdb36ee153fd8e43c3176
juljul22 <julie.joan@web.de>
2025-10-28 19:25:38 +0100

    Update application.py

--- application.py
+++ application.py
@@ -1,14 +1,11 @@
-from flask import Flask, render_template, url_for
+from flask import Flask
+from capp.home.routes import home
+from capp.methodology.routes import methodology
+from capp.carbon_app.routes import carbon_app
+
 application = Flask(__name__)
 
-@application.route('/')
-@application.route('/home')
-def home():
-    return render_template('home.html')
-
-@application.route('/methodology')
-def methodology():
-    return render_template('methodology.html')
-
-if __name__=='__main__':
-    application.run(debug=True)  
+# Blueprints registrieren
+application.register_blueprint(home)
+application.register_blueprint(methodology)
+application.register_blueprint(carbon_app)
