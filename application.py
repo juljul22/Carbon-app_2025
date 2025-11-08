d4ae0e51a5b3c4e23b73a35fdc28b86ff4fbf095
juljul22 <julie.joan@web.de>
2025-11-08 16:38:52 +0100

    Update application.py

--- application.py
+++ application.py
@@ -1,18 +1,4 @@
-from flask import Flask, render_template, url_for
-application = Flask(__name__)
-
-@application.route('/')
-@application.route('/home')
-def home():
-    return render_template('home.html')
-
-@application.route('/methodology')
-def methodology():
-    return render_template('methodology.html')
-
-@application.route('/aboutus')
-def aboutus():
-    return render_template ('aboutus.html')
+from capp import application
 
 if __name__=='__main__':
-    application.run(debug=True)  
+  application.run(debug=True)
