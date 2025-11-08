15653c7a3865094cd71af76b961b3bbdcdfcef98
juljul22 <julie.joan@web.de>
2025-11-09 00:03:45 +0100

    Delete capp directory

--- capp/calculator/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template, Blueprint
-
-calculator=Blueprint('calculator',__name__)
-
-@calculator.route('/calculator')
-def calculator_home():
-  return render_template('calculator.html', title='calculator')
