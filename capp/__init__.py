3c05b499392d9a98312bf93faf555e5a7225e90b
smazouz1810-ui <smazouz.1810@gmail.com>
2025-11-10 09:03:53 +0100

    Remove extra blank lines in __init__.py

--- capp/__init__.py
+++ capp/__init__.py
@@ -6,7 +6,6 @@ from capp.home.routes import home
 from capp.calculator.routes import calculator
 from capp.aboutus.routes import aboutus
 
-
 application.register_blueprint(home)
 application.register_blueprint(calculator)
 application.register_blueprint(aboutus)
