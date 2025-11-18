227d6590ee0d6b9ac2eb7d5786e9325dd11a990f
juljul22 <julie.joan@web.de>
2025-11-18 13:58:05 +0100

    Update __init__.py

--- capp/__init__.py
+++ capp/__init__.py
@@ -6,11 +6,11 @@ import os
 
 application = Flask(__name__)
 
-### Code GitHub
-application.config['SECRET_KEY'] = os.environ['SECRET_KEY']  
-DBVAR = f"postgresql://{os.environ['RDS_USERNAME']}:{os.environ['RDS_PASSWORD']}@{os.environ['RDS_HOSTNAME']}/{os.environ['RDS_DB_NAME']}"
-application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR 
-application.config['SQLALCHEMY_BINDS'] ={'transport': DBVAR}
+# application.config['SECRET_KEY'] = os.environ['SECRET_KEY']  
+application.config['SECRET_KEY'] = '3oueqkfdfas8ruewqndr8ewrewrouewrere44554'
+
+application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
+application.config['SQLALCHEMY_BINDS'] ={'transport': 'sqlite:///transport.db'}
 
 
 db = SQLAlchemy(application)
@@ -29,3 +29,4 @@ application.register_blueprint(calculator)
 application.register_blueprint(aboutus)
 
 application.register_blueprint(users)
+
